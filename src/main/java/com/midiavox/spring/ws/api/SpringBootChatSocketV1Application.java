5b3f467d2564907767fdb356ace5db0b1b989640
Maycon R Santana <maycon_gus2009@hotmail.com>
2020-07-22 14:09:36 -0300

    Primeiro commit

---
+++ src/main/java/com/midiavox/spring/ws/api/SpringBootChatSocketV1Application.java
@@ -0,0 +1,13 @@
+package com.midiavox.spring.ws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatSocketV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChatSocketV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/midiavox/spring/ws/api/SpringBootChatSocketV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midiavox.spring.ws.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatSocketV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
